ab2e15014d00de3ebaf91472f772ae3cd51dfb21
yechang <215372535@qq.com>
2017-10-31 19:28:04 +0800

    init

--- client/config/api.config.js
+++ client/config/api.config.js
@@ -1,5 +1,5 @@
 const isPro = Object.is(process.env.NODE_ENV, 'production')
 
 module.exports = {
-  baseUrl: isPro ? 'http://www.vnshop.cn/api/' : 'api/'
+  baseUrl: isPro ? 'http://shop.zjyechang.top/api' : 'api/'
 }
